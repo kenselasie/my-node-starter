723344af6c1f64afe3415a6f420aad3371f8d416
kenselasie <kennedyadzoyi98@gmail.com>
2021-04-20 03:13:10 +0000

    refactored orders

--- api/controllers/ordersController.js
+++ api/controllers/ordersController.js
@@ -1,4 +1,5 @@
 import Order from '../models/orderModel.js'
+import Product from '../models/productModel.js'
 
 
 const getAllOrders = async (req, res) => {
@@ -6,19 +7,85 @@ const getAllOrders = async (req, res) => {
         let orders = await Order.find({})
             .populate('product', 'name')
             .select('product quantity _id')
-        console.log(orders)
         if (orders) {
             res.status(200).json({
                 message: 'Orders successfully fetched',
                 orders
             })
         }
-    } catch(err) {
+    } catch (err) {
         res.status(500).json({
             error: err
         })
     }
 }
 
+const getOrderById = async (req, res) => {
+    try {
+        const { order_id } = req.params
+        let order = await Order.findById(order_id).populate('product', 'name')
+        if (!order) return res.status(404).json({ message: 'No such order' })
 
-export { getAllOrders }
+        return res.status(200).json({
+            message: 'Success',
+            data: order
+        })
+    }
+    catch (err) {
+        res.status(500).json({
+            err
+        })
+    }
+}
+
+const addOrder = async (req, res) => {
+    try {
+        const { productId, quantity } = req.body
+        const product = await Product.findById(productId)
+        if (!product) {
+            return res.status(404).json({
+                message: 'No order with that id',
+            })
+        }
+        const order = new Order({
+            product: productId,
+            quantity: quantity
+        })
+        await order.save()
+        res.status(201).json({
+            message: 'Order successfully created',
+            order
+        })
+    }
+    catch (err) {
+        res.status(500).json({
+            error: err
+        })
+    }
+}
+
+const deleteOrderById = async (req, res) => {
+    try {
+        const { order_id } = req.params
+
+        let order = await Order.findById(order_id)
+        if (!order) return res.status(404).json({
+            message: 'No order with that id exits to be deleted'
+        })
+
+        let remove = await Order.remove({ _id: order_id })
+        if (!remove) return res.status(404).json({ message: 'No order with that id' })
+
+        return res.status(200).json({
+            message: 'Order Deleted!'
+        })
+    }
+    catch (err) {
+        res.status(500).json({
+            err
+        })
+    }
+}
+
+
+export { getAllOrders, getOrderById, deleteOrderById, addOrder }
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,84 +1,24 @@
 import express from 'express'
 
-import Order from '../models/orderModel.js'
-import { getAllOrders } from '../controllers/ordersController.js'
-import Product from '../models/productModel.js'
+import { getAllOrders, addOrder, getOrderById, deleteOrderById } from '../controllers/ordersController.js'
 import checkAuth from '../middleware/check-auth.js'
 
 const router = express.Router()
 
-router.get('/', checkAuth, (req, res) => {
+router.get('/',  (req, res) => {
     return getAllOrders(req, res)
 })
 
-router.post('/', checkAuth, (req, res, next) => {
-    Product.findById(req.body.productId)
-    .then(product => {
-        if (!product) {
-            res.status(404).json({
-                message: 'No product with that id',
-            })
-        }
-        const order = new Order({
-            product: req.body.productId,
-            quantity: req.body.quantity
-        })
-    
-        return order.save()       
-    }).then(order => {
-        console.log({
-            message: 'POST /orders',
-            order
-        })
-        res.status(201).json({
-            message: 'Order successfully created',
-            order
-        })
-    }).catch(err => {
-        res.status(500).json({
-            error: err
-        })
-    })
-
+router.post('/',  (req, res) => {
+    return addOrder(req, res)
 })
 
-router.get('/:orderId', checkAuth, (req, res, next) => {
-    Order.findById(req.params.orderId)
-    .populate('product', 'name')
-    .exec()
-    .then(order => {
-        if(!order) {
-            res.status(404).json({
-                message: 'No such order'       
-            })
-        } else {
-            console.log({
-                message: 'GET /order by Id',
-                order
-            })
-
-            res.status(200).json({
-                order       
-            })
-        }
-    }).catch(err => {
-        res.status(500).json({
-            err       
-        })
-    })
+router.get('/:order_id',  (req, res) => {
+    return getOrderById(req, res)
 })
 
-router.delete('/:orderId', checkAuth, (req, res, next) => {
-    Order.remove({_id: req.params.orderId}).exec()
-    .then(order => {
-        res.status(200).json({
-            message: 'Order Deleted!'       
-        })
-    }).catch(err => {
-        res.status(500).json({
-            err       
-        })
-    })
+router.delete('/:order_id',  (req, res) => {
+    return deleteOrderById(req, res)
 })
 
 export default router
