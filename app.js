bc8a0b815705cb3a45dbf8ed8a61148c387c902a
kenselasie <kennedyadzoyi98@gmail.com>
2021-04-20 03:12:50 +0000

    removed body parser and modified mongodb

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from "express"
 import cors from "cors"
-import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import path from "path"
 import dotenv from "dotenv"
@@ -16,15 +15,19 @@ dotenv.config()
 // }
 
 //Database connection
-mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/node-rest-db', { useNewUrlParser: true, useUnifiedTopology: true }, )
-.then(() => console.log('You are now connected to the node-rest-db Database!'))
-.catch(err => console.error('Something went wrong connecting to the database', err))
+mongoose
+    .connect('mongodb://localhost/node-rest-db', { 
+        useCreateIndex: true,
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+    })
+    .then(() => console.log('You are now connected to the node-rest-db Database!'))
+    .catch(err => console.error('Something went wrong connecting to the database', err))
 
 app.use(morgan('dev'))
 app.use('/uploads', express.static('uploads'))
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 
 app.use(cors())
